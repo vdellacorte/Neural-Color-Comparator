ecbbd3a1e96c0e374acd3822e38623af8602ee3d
vincentdc95 <slv.vincent95@gmail.com>
2018-11-17 12:05:54 +0100

    added dataset. added positioning of the window to show master and the copy placed side by side

--- printRGB.m
+++ printRGB.m
@@ -1,8 +1,9 @@
-function [] = printRGBfromSpectrum(spectrum,titolo)
+function [] = printRGBfromSpectrum(spectrum,titolo, windowPosition)
     spectrum = spectrum*100;
     rgb = roo2rgb(spectrum','srgb',380:1:800);
     fig = figure('visible', 'on');
+    fig.OuterPosition = windowPosition;
     rect = rectangle('FaceColor', rgb);
     title(titolo);
-    hold on
+
 end
--- testNoise.m
+++ testNoise.m
@@ -1,6 +1,7 @@
 function [deltaE] = testNoise(spectra)
-    noisySpectrum = addNoise(spectra,100)
-    printRGB(spectra(:,100),'master');
-    printRGB(noisySpectrum,'copy');
+    noisySpectrum = addNoise(spectra,100);
+    masterSpectrum = spectra(:,100);
+    printRGB(masterSpectrum, 'master', [201.0000  334.6000  574.4000  409.6000]);
+    printRGB(noisySpectrum,'copy', [750.0000  334.6000  574.4000  409.6000]);
     deltaE = computeDeltaE(spectra(:,100),noisySpectrum);
 end
