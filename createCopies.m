7ef7b5c018f801173f8c0b2a0f89c3e5e6e67349
vincentdc95 <slv.vincent95@gmail.com>
2018-11-21 16:12:55 +0100

    added function to create all copies

---
+++ createCopies.m
@@ -0,0 +1,18 @@
+function [spectraCopies] = createCopies(spectra, numCopies, seed)
+    
+spectraCopies = zeros(421, 1269*numCopies);
+numRangesForNoise = 3;
+rng(seed);
+
+for i = 1:1269
+    for j = 1:numCopies
+        
+        spectraCopies(:, (i-1)*numCopies + j) = addNoise( spectra, i, numRangesForNoise);
+    end
+end
+
+% generiamo una matrice spectraCopies [421; 1269*copie] in cui ho numCopies
+% copie di ogni master. Ogni set di numCopies colonne rappresentano le
+% copie di uno stesso master
+
+end
